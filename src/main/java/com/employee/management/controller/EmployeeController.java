4d8e7731e2256e94a5776a2ecca5c78113f80513
gbalu555 <balugopinath93@gmail.com>
2024-07-04 22:41:48 +0530

    Added logger for the application

--- src/main/java/com/employee/management/controller/EmployeeController.java
+++ src/main/java/com/employee/management/controller/EmployeeController.java
@@ -7,35 +7,44 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
+    private static final Logger logger = LoggerFactory.getLogger(EmployeeController.class);
+
     @Autowired
     private EmployeeService employeeService;
 
     @GetMapping
     public List<Employee> getAllEmployees() {
+        logger.info("Received request to fetch all employees");
         return employeeService.getAllEmployees();
     }
 
     @GetMapping("/{id}")
     public Employee getEmployeeById(@PathVariable Long id) {
+        logger.debug("Received request to fetch employee with id: {}", id);
         return employeeService.getEmployeeById(id);
     }
 
     @PostMapping
     public Employee createEmployee(@RequestBody Employee employee) {
+        logger.debug("Received request to create new employee: {}", employee.getName());
         return employeeService.createEmployee(employee);
     }
 
     @PutMapping("/{id}")
     public Employee updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails) {
+        logger.debug("Received request to update employee with id: {}", id);
         return employeeService.updateEmployee(id, employeeDetails);
     }
 
     @DeleteMapping("/{id}")
     public void deleteEmployee(@PathVariable Long id) {
+        logger.debug("Received request to delete employee with id: {}", id);
         employeeService.deleteEmployee(id);
     }
 }
